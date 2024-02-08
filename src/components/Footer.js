d67af55eff5cd206ae1a59d8863352921b8a4b7d
driesstandaert <driesstandaert@gmail.com>
2024-02-08 16:32:30 +0000

    Remove deprecated social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,21 +9,21 @@ const Footer = () => {
             <a href="mailto:dries.standaert@gmail.com" className="c-link">dries.standaert@gmail.com</a>
           </p>
           <ul className="o-list c-social__list u-text-center">
-            <li className="o-list__item c-social__list-item">
+            {/*<li className="o-list__item c-social__list-item">
               <a href="http://twitter.com/driesstandaert" target="_blank" rel="noreferrer" className="c-social__link c-social__link--twitter">Twitter</a>
-            </li>
+            </li>*/}
             <li className="o-list__item c-social__list-item">
               <a href="https://www.linkedin.com/in/dries-standaert-67635383/" target="_blank" rel="noreferrer" className="c-social__link c-social__link--linkedin">LinkedIn</a>
             </li>
-            <li className="o-list__item c-social__list-item">
+            {/*<li className="o-list__item c-social__list-item">
               <a href="https://dribbble.com/driesstandaert" target="_blank" rel="noreferrer" className="c-social__link c-social__link--dribbble">dribbble</a>
-            </li>
+            </li>*/}
             <li className="o-list__item c-social__list-item">
               <a href="http://codepen.io/driesstandaert/" target="_blank" rel="noreferrer" className="c-social__link c-social__link--codepen">Codepen</a>
             </li>
-            <li className="o-list__item c-social__list-item">
+            {/*<li className="o-list__item c-social__list-item">
               <a href="https://github.com/driesstandaert" target="_blank" rel="noreferrer" className="c-social__link c-social__link--github">Github</a>
-            </li>
+            </li>*/}
           </ul>
         </div>
       </div>
